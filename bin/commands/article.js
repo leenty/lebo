aca2b27a6fb26d5abd9a6e6124c9d0dc92a52411
leenty <leenty@qq.com>
2017-05-04 15:38:32 +0800

    添加article系列命令
    使用 lebo -h 查看全局命令
    使用 lebo article -h 查看article子命令
    
      lebo article -h
    
      Usage: article [options]
    
      create article and generate article's route
    
      Options:
    
        -h, --help              output usage information
        -r, --render            generate article's route
        -c, --create <article>  create a article named <article>
        -m, --mkdir             create articles directory

---
+++ bin/commands/article.js
@@ -0,0 +1,24 @@
+const Article = require('../../lib/article')
+
+module.exports = function (program) {
+  program
+    .command('article')
+    .description('create article and generate article\'s route')
+    .option('-r, --render', 'generate article\'s route')
+    .option('-c, --create <article>', 'create a article named <article>')
+    .option('-m, --mkdir', 'create articles directory')
+    .action(function (command) {
+      command.mkdir &&
+        Article.dir.create()
+          .then(console.log)
+          .catch(console.log)
+      command.create &&
+        Article.article.create(command.create)
+          .then(console.log)
+          .catch(console.log)
+      command.render &&
+        Article.router.create()
+          .then(info => console.log(...info))
+          .catch(err => console.log(...err))
+    })
+}
