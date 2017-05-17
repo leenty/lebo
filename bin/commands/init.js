644d1b31c877f1b9bf709691bce2846c28b80e01
leenty <leenty@qq.com>
2017-05-17 18:20:35 +0800

    init 模块添加询问信息方法

--- bin/commands/init.js
+++ bin/commands/init.js
@@ -5,15 +5,14 @@ module.exports = function (program) {
     .command('init <name>')
     .description('download and init blog template')
     .action(command => {
-      // const repl = require('repl');
-      // // 一个 Unix 风格的提示符
-      // repl.start('$ ');
-      
       const spinner = require('../../lib/spinner')
-      spinner('downloading template', Init.download(command))
-        .then(() => spinner('changing template', Init.initProject(command)))
-        .catch(() => {})
+      // spinner('downloading template', Init.download(command))
+      //   .then(() => spinner('changing template', Init.initProject(command)))
+      //   .catch(() => {})
 
+      Init.initProject(command)
+        .then(console.log)
+        .catch(console.log)
       // spinner('downloading template', Init.initProject(command))
       //   .catch(() => {})
     })
